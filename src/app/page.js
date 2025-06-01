91efb3fd39a722f6948ce783a1422111438ca948
fatihsenyigit <fsenyigit8@gmail.com>
2025-06-01 20:07:46 +0700

    tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div className="relative h-screen w-full ]">
+    <div className="relative h-screen w-full bg-[url(/images/hero.jpg)]]">
       
      merhanba
     </div>
